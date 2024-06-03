653d86f7d91fff7c93c2671f9d44a14c091db8b9
BentumYaw001 <richmondbentum2020@gmail.com>
2024-06-03 01:56:27 -0700

    coloring preview elements

--- my-project/src/components/preview.jsx
+++ my-project/src/components/preview.jsx
@@ -10,7 +10,7 @@ function Preview({ markdown }) {
       <div className="w-full bg-gray-900 p-3 text-gray-400 uppercase tracking-wider">
         Preview
       </div>
-      <div className='p-6 prose prose-invert text-a-' dangerouslySetInnerHTML={{ __html: parsed }} />
+      <div className='p-6 prose prose-invert' dangerouslySetInnerHTML={{ __html: parsed }} />
     </div>
   );
 }
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -17,6 +17,57 @@ export default {
       fontFamily: {
         'roboto': ['Roboto', 'sans-serif'],
       },
+
+      typography: (theme) => ({
+        invert: {
+          css: {
+            '--tw-prose-body': theme('colors.gray.300'),
+            '--tw-prose-headings': theme('colors.white'),
+            '--tw-prose-lead': theme('colors.gray.400'),
+            '--tw-prose-links': theme('colors.white'),
+            '--tw-prose-bold': theme('colors.white'),
+            '--tw-prose-counters': theme('colors.gray.500'),
+            '--tw-prose-bullets': '#E46643', // List bullets
+            '--tw-prose-hr': theme('colors.gray.200'),
+            '--tw-prose-quotes': theme('colors.gray.400'),
+            '--tw-prose-quote-borders': '#E46643', // Blockquote borders
+            '--tw-prose-captions': theme('colors.gray.500'),
+            '--tw-prose-code': theme('colors.gray.400'),
+            '--tw-prose-pre-code': theme('colors.gray.400'),
+            '--tw-prose-pre-bg': theme('colors.gray.800'),
+            '--tw-prose-th-borders': theme('colors.gray.700'),
+            '--tw-prose-td-borders': theme('colors.gray.700'),
+            '--tw-prose-invert-body': theme('colors.gray.300'),
+            '--tw-prose-invert-headings': theme('colors.white'),
+            '--tw-prose-invert-lead': theme('colors.gray.400'),
+            '--tw-prose-invert-links': theme('colors.white'),
+            '--tw-prose-invert-bold': theme('colors.white'),
+            '--tw-prose-invert-counters': theme('colors.gray.500'),
+            '--tw-prose-invert-bullets': '#E46643', // List bullets
+            '--tw-prose-invert-hr': theme('colors.gray.200'),
+            '--tw-prose-invert-quotes': theme('colors.gray.400'),
+            '--tw-prose-invert-quote-borders': '#E46643', // Blockquote borders
+            '--tw-prose-invert-captions': theme('colors.gray.500'),
+            '--tw-prose-invert-code': theme('colors.gray.400'),
+            '--tw-prose-invert-pre-code': theme('colors.gray.400'),
+            '--tw-prose-invert-pre-bg': theme('colors.gray.800'),
+            '--tw-prose-invert-th-borders': theme('colors.gray.700'),
+            '--tw-prose-invert-td-borders': theme('colors.gray.700'),
+            // Custom styles for headers with 6#
+            'h6': {
+              color: '#E46643',
+            },
+            // Custom styles for bullets in list items
+            'ul > li::before': {
+              backgroundColor: '#E46643',
+            },
+
+            'blockquote': {
+              fontStyle: 'normal', // Remove italics from blockquotes
+            },
+          },
+        },
+      })
     },
   },
   // eslint-disable-next-line no-undef
